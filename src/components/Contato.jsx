d2b0ad057b507f7bb34f0e543acb36deb62cc06b
Ewerton Henrique <ewertonhsenai@gmail.com>
2026-02-04 16:06:28 -0300

    Update Contato.jsx

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -6,7 +6,7 @@ export default function Contato() {
         <h3>Contatos:</h3>
         <a href="https://github.com/Ton44131">GitHub</a><br />
         <a href="https://www.linkedin.com/in/ewerton-henrique-marques-da-silva-0b4060358/">Linkedin</a>
-        <p>E-mail: ewertonhsenai@gmail.com</p>
+        <p>E-mail: em.ewertont@gmail.com</p>
       </div>
 
     </section>
